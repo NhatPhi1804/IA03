8c98a0380a813a78713fcd7f7cc92e6f6fecfeaa
NhatPhi1804 <kenshiro.pn@gmail.com>
2024-11-05 19:24:28 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
@@ -12,7 +14,7 @@ async function bootstrap() {
   }));
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: configService.get<string>('CORS_ORIGIN'),
     credentials: true,
   });
 
